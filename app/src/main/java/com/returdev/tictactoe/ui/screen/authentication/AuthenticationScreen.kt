242211460aceaa26968a5e1eb7a3d411a66301aa
ReturDev <sergiomm50@gmail.com>
2024-10-29 22:50:10 +0100

    feat: Add SignInButtonsColumn composable for displaying sign-in options
    - Created a SignInButtonsColumn composable to display a column of sign-in buttons for various authentication providers.
    - Each button includes a text label and icon, with support for customized layout through a modifier.
    - Added a lambda function to handle sign-in actions based on the selected provider and an isEnabled parameter to toggle button availability.
    - Included divider and title styling to enhance UI clarity and alignment.

--- app/src/main/java/com/returdev/tictactoe/ui/screen/authentication/AuthenticationScreen.kt
+++ app/src/main/java/com/returdev/tictactoe/ui/screen/authentication/AuthenticationScreen.kt
@@ -1,28 +1,97 @@
 package com.returdev.tictactoe.ui.screen.authentication
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.returdev.tictactoe.R
+import com.returdev.tictactoe.ui.screen.authentication.model.AuthProvider
 
+
+/**
+ * Composable function that displays a column of sign-in buttons.
+ *
+ * @param modifier Optional modifier for customizing the layout of this composable.
+ * @param authProviders List of available authentication providers to display as sign-in options.
+ * Each provider includes a text and icon resource.
+ * @param onSignIn Lambda function triggered when the user selects a sign-in provider.
+ * It receives the selected `AuthProvider` as a parameter.
+ * @param isEnabled Boolean flag indicating whether the sign-in buttons are enabled or disabled.
+ */
+@Composable
+private fun SignInButtonsColumn(
+    modifier : Modifier = Modifier,
+    authProviders : List<AuthProvider>,
+    onSignIn : (AuthProvider) -> Unit,
+    isEnabled : Boolean
+){
+
+    Card(
+        modifier = modifier.fillMaxWidth()
+    ) {
+
+        Column(
+            modifier = Modifier.padding(vertical = 24.dp, horizontal = 16.dp)
+        ) {
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.authentication_sign_in_to_play),
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            HorizontalDivider(modifier = Modifier.padding(top = 16.dp, bottom = 24.dp))
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+
+                authProviders.forEach{ authProvider ->
+
+                    AuthenticationButton(
+                        text = stringResource(id = authProvider.stringRes),
+                        icon = ImageVector.vectorResource(authProvider.iconRes),
+                        skipIconTint = authProvider.skipIconTint,
+                        isEnabled = isEnabled
+                    ) {
+                        onSignIn(authProvider)
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
 
 /**
  * A private composable function to display a button for authentication.
@@ -72,26 +141,32 @@ private fun AuthenticationButton(
 private fun AuthenticationScreenPrev(){
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize().padding(16.dp)
     ){
-    
-        AuthenticationButton(
-            text = "Continue with google",
-            icon = Icons.Default.Warning,
-            skipIconTint = false,
-            isEnabled = true
-        ) {
-            
-        }
-Spacer(modifier = Modifier.height(24.dp))
-        AuthenticationButton(
-            text = "Continue with Facebook",
-            icon = Icons.Default.Warning,
-            skipIconTint = true,
-            isEnabled = true
-        ) {
 
-        }
+        SignInButtonsColumn(
+            authProviders = AuthProvider.entries,
+            onSignIn = { aut -> },
+            isEnabled = true
+        )
+
+//        AuthenticationButton(
+//            text = "Continue with google",
+//            icon = Icons.Default.Warning,
+//            skipIconTint = false,
+//            isEnabled = true
+//        ) {
+//
+//        }
+//Spacer(modifier = Modifier.height(24.dp))
+//        AuthenticationButton(
+//            text = "Continue with Facebook",
+//            icon = Icons.Default.Warning,
+//            skipIconTint = true,
+//            isEnabled = true
+//        ) {
+//
+//        }
         
     }
     
