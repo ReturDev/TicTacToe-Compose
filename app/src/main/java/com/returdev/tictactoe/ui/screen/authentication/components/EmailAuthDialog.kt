06b59fa199a6e001cc895eae7bd6a9a070de1811
sergiomm50 <sergiomm50@gmail.com>
2024-12-01 19:44:00 +0100

    feat: Add GenericPasswordTextField composable for password input with toggle visibility
    - Implemented GenericPasswordTextField to provide a password input field with the ability to toggle visibility.
    - Supports optional error messages, labels, and supporting text for enhanced user feedback.
    - Added functionality to toggle password visibility using a button and display error or supporting text beneath the field.
    - Included `isEnabled` flag to control the interactivity of the password field.

--- app/src/main/java/com/returdev/tictactoe/ui/screen/authentication/components/EmailAuthDialog.kt
+++ app/src/main/java/com/returdev/tictactoe/ui/screen/authentication/components/EmailAuthDialog.kt
@@ -3,16 +3,89 @@ package com.returdev.tictactoe.ui.screen.authentication.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.returdev.tictactoe.R
+import com.returdev.tictactoe.ui.screen.authentication.model.AuthenticationErrorType
+
+
+/**
+ * A composable function that displays a password input field with the ability to toggle password visibility.
+ * It supports an optional error message, a label, and a supporting text. The password visibility can be toggled
+ * with a button that shows or hides the password as the user types.
+ *
+ * @param modifier The modifier to be applied to the [TextField] composable. Defaults to [Modifier].
+ * @param label The label to display above the text field, describing the expected input (e.g., "Password").
+ * @param value The current value of the password input field.
+ * @param passwordError An optional error type that, if non-null, will display an error message below the text field.
+ * @param supportingText Optional additional text to display beneath the text field. It will be shown if no error is present.
+ * @param isEnabled A boolean value indicating whether the password field is interactive or disabled.
+ * @param onValueChange A lambda function that is invoked whenever the password input value changes.
+ * It takes the new value of the text field as a parameter.
+ */
+@Composable
+private fun GenericPasswordTextField(
+    modifier : Modifier = Modifier,
+    label : String,
+    value : String,
+    passwordError : AuthenticationErrorType?,
+    supportingText : String?,
+    isEnabled : Boolean,
+    onValueChange : (String) -> Unit
+) {
+
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    TextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        visualTransformation = if (isPasswordVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
+        label = { Text(text = label) },
+        isError = passwordError != null,
+        supportingText = {
+            Text(text = passwordError?.let { stringResource(it.stringRes) }
+                ?: supportingText.orEmpty())
+        },
+        trailingIcon = {
+            IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                if (isPasswordVisible) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_password_visibility_off),
+                        contentDescription = stringResource(R.string.authentication_email_hide_password)
+                    )
+                } else {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_password_visibility),
+                        contentDescription = stringResource(R.string.authentication_email_show_password)
+                    )
+                }
+            }
+        },
+        enabled = isEnabled
+    )
+
+}
+
 
 /**
  * A composable function that displays a row of text elements at the bottom of the authentication screen.
@@ -42,7 +115,7 @@ private fun BottomChangeAuthText(
                 id = if (isSignUpCurrentView)
                     R.string.authentication_email_have_account
                 else
-                    R.string.athentication_email_have_not_account
+                    R.string.authentication_email_have_not_account
             )
         )
 
@@ -67,16 +140,25 @@ private fun BottomChangeAuthText(
 
 @Preview
 @Composable
-private fun EmailAuthDialog(){
+private fun EmailAuthDialog() {
 
     val isSignUp = remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier.fillMaxSize()
-    ){
-        BottomChangeAuthText(isSignUpCurrentView = isSignUp.value) {
-            isSignUp.value = it
-        }
+    ) {
+//        BottomChangeAuthText(isSignUpCurrentView = isSignUp.value) {
+//            isSignUp.value = it
+//        }
+//        PasswordTextField(
+//            modifier = Modifier,
+//            label = "Password",
+//            value = "hola",
+//            supportingText = "Escribe la contraseña",
+//            isEnabled = true,
+//            passwordError = AuthenticationErrorType.Password(R.string.authentication_as_guest),
+//            onValueChange = {}
+//        )
     }
 
 }
