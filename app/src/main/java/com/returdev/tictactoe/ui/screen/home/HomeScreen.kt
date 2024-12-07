09fa4044e8ff72e00c29c3ced02295778b20d97f
sergiomm50 <sergiomm50@gmail.com>
2024-12-07 20:53:26 +0100

    feat: Add HomeButtonsMenu composable for game interaction UI
    - Introduced `HomeButtonsMenu` to manage the game code input, joining an existing game, and creating a new game.
    - Includes an `OutlinedTextField` for entering or pasting a game code with a clearable field and paste functionality.
    - Adds two buttons for joining a game and creating a new game, with validation for input and state control.

--- app/src/main/java/com/returdev/tictactoe/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/returdev/tictactoe/ui/screen/home/HomeScreen.kt
@@ -1,19 +1,136 @@
 package com.returdev.tictactoe.ui.screen.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.returdev.tictactoe.R
 
+/**
+ * A composable that displays a menu for joining a game or creating a new one.
+ * It contains a text field for entering a game code, buttons to join or create a game,
+ * and an option to paste a previously copied game code.
+ *
+ * @param modifier A [Modifier] to customize the layout and appearance of the menu. The default is [Modifier].
+ * @param isEnabled A boolean value that determines whether the text field and buttons are enabled or disabled.
+ * @param pasteGameCode A lambda function that returns a string representing the game code, or null if no code is available to paste.
+ * @param joinToGame A lambda function invoked when the user attempts to join a game using the entered game code.
+ * @param createNewGame A lambda function invoked when the user attempts to create a new game.
+ */
+@Composable
+private fun HomeButtonsMenu(
+    modifier : Modifier = Modifier,
+    isEnabled : Boolean,
+    pasteGameCode : () -> String?,
+    joinToGame : (String) -> Unit,
+    createNewGame : () -> Unit
+) {
+
+    var gameCode by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(0.95f),
+            value = gameCode,
+            onValueChange = {
+                if (gameCode.length <= 50){
+                    gameCode = it
+                }
+            },
+            singleLine = true,
+            placeholder = {
+                Text(
+                    text = stringResource(R.string.home_screen_game_code_placeholder)
+                )
+            },
+            trailingIcon = {
+                if (gameCode.isNotEmpty()){
+                    IconButton(
+                        onClick = { gameCode = "" }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Clear,
+                            contentDescription = stringResource(R.string.home_screen_clear_game_code)
+                        )
+                    }
+                }
+            },
+            leadingIcon = {
+                IconButton(
+                    onClick = {
+                        pasteGameCode()?.let {
+                            gameCode = it
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_paste),
+                        contentDescription = stringResource(R.string.home_screen_paste_code)
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Send
+            ),
+            keyboardActions = KeyboardActions(
+                onSend = {joinToGame(gameCode)}
+            ),
+            enabled = isEnabled
+        )
+
+        HomeMenuButton(
+            text = stringResource(R.string.home_screen_join_to_game),
+            isEnabled = isEnabled && gameCode.isNotEmpty()
+        ) { joinToGame(gameCode) }
+
+        Text(
+            text = stringResource(R.string.home_screen_or_divider),
+            fontWeight = FontWeight.Bold
+        )
+
+        HomeMenuButton(
+            text = stringResource(R.string.home_screen_new_game),
+            isEnabled = isEnabled,
+            onClick = createNewGame
+        )
+
+    }
+
+}
 
 /**
  * A composable function that displays a button with the provided text and a click action.
@@ -55,6 +172,7 @@ private fun HomePrev() {
                 .padding(16.dp)
         ) {
 
+            HomeButtonsMenu(isEnabled = true, pasteGameCode = {""}, joinToGame = {}, createNewGame = {})
 
         }
     }
