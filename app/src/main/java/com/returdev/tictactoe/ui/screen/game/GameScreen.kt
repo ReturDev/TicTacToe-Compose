b4092bd6e0508c2cf767805ce155cc132d27af9f
sergiomm50 <sergiomm50@gmail.com>
2025-02-02 18:20:59 +0100

    feat: Add `drawO` function for animated circular "O" drawing on canvas
    - Implemented `drawO` function to render an animated circular "O" shape based on progress and customizable color.
    - Utilized `drawArc` with negative sweep angles for smooth clockwise arc rendering.
    - Applied stroke properties with rounded caps to maintain smooth visual aesthetics.

--- app/src/main/java/com/returdev/tictactoe/ui/screen/game/GameScreen.kt
+++ app/src/main/java/com/returdev/tictactoe/ui/screen/game/GameScreen.kt
@@ -4,40 +4,48 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
 
 
+/**
+ * Draws a circular shape on the canvas based on the given progress and color.
+ * @receiver DrawScope The drawing context to render the "O" shape.
+ * @param progress A value between `0f` and `1f` indicating the drawing progress of the "O".
+ *                 - At `0f`, no part of the "O" is drawn.
+ *                 - At `1f`, the full circular "O" is drawn.
+ * @param color The color used to draw the "O".
+ */
+private fun DrawScope.drawO(
+    progress: Float,
+    color: Color
+) {
+    drawArc(
+        color = color,
+        startAngle = -90f,
+        sweepAngle = -360f * progress,
+        useCenter = false,
+        style = Stroke(
+            width = this.size.width / 3,
+            cap = StrokeCap.Round
+        )
+    )
+}
 
 
 /**
@@ -115,7 +123,8 @@ private fun GameScreenPreview(){
                 Canvas(
                     modifier = Modifier.size(50.dp).padding(10.dp)
                 ){
-                    drawX(animatedFloat.value, Color.Red)
+//                    drawX(animatedFloat.value, Color.Red)
+                    drawO(animatedFloat.value, Color.Red)
                 }
             }
 
